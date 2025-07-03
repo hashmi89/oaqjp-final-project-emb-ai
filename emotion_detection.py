b04dd944c53bf66e93ad8725bec15752096c2ec6
Sikandar Hashmi <sikandar.salman@gmail.com>
2025-07-02 21:24:26 -0400

    task 3 completed

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,33 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = { "raw_document": { "text": text_to_analyze } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)
-    return response.text
+    
+    #Parsing the JSON response from API
+    formatted_response = json.loads(response.text)
+
+    #Extracting set of emotions from the response
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+    #Finding dominant emotion
+    emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
+    highest_emotion = max(emotion_scores, key=emotion_scores.get)
+    highest_score = emotion_scores[highest_emotion]
+
+    #Returning a dictionary containing emotion analysis
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': highest_emotion
+    }
